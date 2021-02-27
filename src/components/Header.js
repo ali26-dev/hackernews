4014e626521bc498d6afffcedf9dd42f6cc2262e
ali26-dev <mohammad.ali26@pm.me>
2021-02-27 21:21:43 +0500

    Final touch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,23 @@
-{/* First component */}
-
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
-    return(
-        <React.Fragment>
-            <h1>Hacker News Clone</h1>
-            <div className="nav-link">
-                <navlink to="/top" activeClassName="active">
-                    Top Stories
-                </navlink>
-
-                <navlink to="/new" activeClassName="active">
-                    Latest Stories
-                </navlink>
-
-                <navlink to="/best" activeClassName="active">
-                    Best Stories
-                </navlink>
-            </div>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <h1>Hacker News Clone</h1>
+      <div className="nav-link">
+        <NavLink to="/top" activeClassName="active">
+          Top Stories
+        </NavLink>
+        <NavLink to="/new" activeClassName="active">
+          Latest Stories
+        </NavLink>
+        <NavLink to="/best" activeClassName="active">
+          Best Stories
+        </NavLink>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Header;
